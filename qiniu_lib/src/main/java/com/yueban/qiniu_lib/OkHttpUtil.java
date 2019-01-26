41be88343c516104fbf7926e61df3562f9fd2fa7
yueban <fbzhh007@gmail.com>
2019-01-26 16:27:59 +0800

    remove okhttp logging

--- qiniu_lib/src/main/java/com/yueban/qiniu_lib/OkHttpUtil.java
+++ qiniu_lib/src/main/java/com/yueban/qiniu_lib/OkHttpUtil.java
@@ -1,7 +1,6 @@
 package com.yueban.qiniu_lib;
 
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,15 +14,7 @@ class OkHttpUtil {
 
     static OkHttpClient getClient() {
         if (mOkHttpClient == null) {
-            final OkHttpClient.Builder builder;
-            if (BuildConfig.DEBUG) {
-                HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-                logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
-                builder = new OkHttpClient.Builder().addInterceptor(logging);
-            } else {
-                builder = new OkHttpClient.Builder();
-            }
-            mOkHttpClient = builder
+            mOkHttpClient = new OkHttpClient.Builder()
                     .readTimeout(30, TimeUnit.SECONDS)
                     .writeTimeout(30, TimeUnit.SECONDS)
                     .build();
